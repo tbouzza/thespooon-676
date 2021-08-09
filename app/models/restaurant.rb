e91d58b9825d5277f5c7bbb6ec08ec85b729c57e
Tarig Bouazzati <tbouzza@gmail.com>
2021-08-09 10:42:45 +0400

    installed bootstrap

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'Controller#Action'
+  # ALL the restaurant
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # Write a restaurant divided in two steps:
+  # # 1. NEW
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+
+  # # One restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # DELETE a restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # # 2. CREATE
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # UPDATE a restaurant
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  resources :restaurants
 end
